4aff644083091e075e4b7e086cfef8011e1463ff
Zakaul Haque <zakihaqster@gmail.com>
2025-10-05 22:33:19 +0530

    Terms and condition update for Google Approval

--- apps/frontend/src/components/auth/register.tsx
+++ apps/frontend/src/components/auth/register.tsx
@@ -206,14 +206,14 @@ export function RegisterAfter({
             'By registering you agree to our'
           )}&nbsp;
           <a
-            href={`https://postiz.com/terms`}
+            href={`https://ameegolabs.com/terms-of-service.html`}
             className="underline hover:font-bold"
           >
             {t('terms_of_service', 'Terms of Service')}
           </a>&nbsp;
           {t('and', 'and')}&nbsp;
           <a
-            href={`https://postiz.com/privacy`}
+            href={`https://ameegolabs.com/privacy-policy.html`}
             className="underline hover:font-bold"
           >
             {t('privacy_policy', 'Privacy Policy')}
